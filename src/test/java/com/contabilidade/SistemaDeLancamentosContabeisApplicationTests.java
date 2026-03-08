ea22c0807bce8a8a12e28857664cf32f31e153c1
Pacatovisk <marcelovianadias@outlook.com>
2026-03-08 18:28:17 -0300

    feat: inicio do projeto.

---
+++ src/test/java/com/contabilidade/SistemaDeLancamentosContabeisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contabilidade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeLancamentosContabeisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
